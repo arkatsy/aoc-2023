18e8a730a886a789302390e839aa62e65ccde59b
Argyris <arkatsy@gmail.com>
2024-03-24 18:11:00 +0200

    feat: day-2 part-2

--- day-2/main.mjs
+++ day-2/main.mjs
@@ -4,12 +4,6 @@ import fs from "node:fs";
 
 const input = fs.readFileSync(path.resolve(import.meta.dirname, "./data"), "utf-8");
 
-const cubes = {
-  red: 12,
-  green: 13,
-  blue: 14,
-};
-
 // Parse the data
 const games = input.split(os.EOL);
 
@@ -34,17 +28,61 @@ const gameSets = games.map((game) => {
   return { id: gameId, sets: gameSets };
 });
 
-// Part 1
-const validGameIds = gameSets.reduce((acc, curr) => {
-  const sets = curr.sets.flat();
+function solvePart1(gameSets) {
+  const cubes = {
+    red: 12,
+    green: 13,
+    blue: 14,
+  };
 
-  const valid = sets.every((set) => cubes[set.color] >= set.count);
+  const validGameIds = gameSets.reduce((acc, curr) => {
+    const sets = curr.sets.flat();
 
-  if (valid) {
-    acc.push(curr.id);
-  }
+    const valid = sets.every((set) => cubes[set.color] >= set.count);
 
-  return acc;
-}, []);
+    if (valid) {
+      acc.push(curr.id);
+    }
 
-console.log(validGameIds.map(Number).reduce((acc, curr) => acc + curr, 0));
+    return acc;
+  }, []);
+
+  const sum = validGameIds.map(Number).reduce((acc, curr) => acc + curr, 0);
+
+  console.log(`Part 1: ${sum}`);
+}
+
+function solvePart2(gameSets) {
+  // Find min set of cubes for each game
+  const mins = gameSets.map((game) => {
+    const sets = game.sets.flat();
+
+    return sets.reduce((acc, curr) => {
+      if (!acc[curr.color]) {
+        // init
+        acc[curr.color] = curr.count;
+      }
+
+      if (Number(acc[curr.color]) < Number(curr.count)) {
+        acc[curr.color] = curr.count;
+      }
+
+      return acc;
+    }, {});
+  });
+
+  // Calc the power of the mins
+  const power = mins.map((min) => {
+    return Object.values(min)
+      .map(Number)
+      .reduce((acc, curr) => acc * curr, 1);
+  });
+
+  // sum
+  const sum = power.reduce((acc, curr) => acc + curr, 0);
+
+  console.log(`Part 2: ${sum}`);
+}
+
+solvePart1(gameSets);
+solvePart2(gameSets);
